6b3f444718e52b52ca62cc67cc508735b7b57f19
jpaz932 <juanpa.aristis@gmail.com>
2025-02-05 20:59:12 -0500

    feat: set up PostgreSQL database with Prisma and implement order management features

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,28 @@
-export class CreateOrderDto {}
+import { OrderStatus } from '@prisma/client';
+import {
+    IsBoolean,
+    IsEnum,
+    IsNumber,
+    IsOptional,
+    IsPositive,
+} from 'class-validator';
+
+export class CreateOrderDto {
+    @IsNumber()
+    @IsPositive()
+    public totalAmount: number;
+
+    @IsNumber()
+    @IsPositive()
+    public totalItems: number;
+
+    @IsEnum(OrderStatus, {
+        message: `Status must be one of the following values: ${Object.values(OrderStatus).join(', ')}`,
+    })
+    @IsOptional()
+    public status: OrderStatus = OrderStatus.PENDING;
+
+    @IsOptional()
+    @IsBoolean()
+    public paid: boolean = false;
+}
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,26 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
+import { PrismaClient } from '@prisma/client';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
-export class OrdersService {
+export class OrdersService extends PrismaClient implements OnModuleInit {
+    private readonly logger = new Logger('OrdersService');
+    async onModuleInit() {
+        await this.$connect();
+        this.logger.log('Connected to the database');
+    }
+
     create(createOrderDto: CreateOrderDto) {
-        return 'This action adds a new order';
+        return this.order.create({
+            data: createOrderDto,
+        });
     }
 
     findAll() {
         return `This action returns all orders`;
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} order`;
-    }
+    async findOne(id: string) {
+        const order = await this.order.findUnique({
+            where: { id },
+        });
 
-    update(id: number, updateOrderDto: UpdateOrderDto) {
-        return `This action updates a #${id} order`;
-    }
+        if (!order) {
+            throw new RpcException({
+                status: HttpStatus.NOT_FOUND,
+                message: `Order with id ${id} not found`,
+            });
+        }
 
-    remove(id: number) {
-        return `This action removes a #${id} order`;
+        return order;
     }
 }
